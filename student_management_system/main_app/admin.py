be044c8804385eb8f8bad7019bddb8a68b80a110
Sneha0201 <snehachaurasia0201@gmail.com>
2026-03-10 14:55:27 +0530

    Update admin.py

--- student_management_system/main_app/admin.py
+++ student_management_system/main_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Admin, Staff, Student
+from .models import Admin,Attendance, Staff, Student
 
 admin.site.register(Admin)
+admin.site.register(Attendance)
 admin.site.register(Staff)
 admin.site.register(Student)
